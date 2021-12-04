30d750e536e49ea492eb8fa7e9602cba045d1891
quincy <quincyouxiang@gmail.com>
2021-12-04 15:51:21 -0500

    add home page and a search button

--- frontend/autog/src/App.js
+++ frontend/autog/src/App.js
@@ -2,16 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import TimeTable from "./components/TimeTable";
 import SearchBar from "./components/SearchBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 function App() {
     return (
         <div className="App">
-            <p className="TimeTable">
-                <TimeTable></TimeTable>
-            </p>
-            <p className="SearchBar">
-                <SearchBar></SearchBar>
-            </p>
+            <div className="HomeBackground">
+                <SearchBar/>
+                <div className="TimeTable">
+                    <TimeTable></TimeTable>
+                </div>
+            </div>
         </div>
     );
 }
